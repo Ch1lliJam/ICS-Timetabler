4124c57c009615140f788be6e95ee25657527e4c
James <dundjam234@gmail.com>
2025-02-14 14:35:00 +0000

    fix old code

--- private/autoload.php
+++ private/autoload.php
@@ -1,7 +1,6 @@
 <?php
 require "../private/connection.php";
 require "../private/functions.php";
-require __DIR__ . '/../vendor/autoload.php'; // Ensure correct path
-use Kigkonsult\Icalcreator\Vcalendar;
+
 
 ?>
--- public/loginpage.php
+++ public/loginpage.php
@@ -1,11 +1,7 @@
 <?php
 // filepath: /d:/Xampp/htdocs/mini website new approach/public/loginpage.php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
-
 require '../private/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
